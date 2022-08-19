3c261fb60dfe7590c9a47ebc1a7b8e5685dd4fe8
derekhirotsu <55931997+derekhirotsu@users.noreply.github.com>
2022-08-19 15:12:27 -0700

    Implement Audio Mixer (#3)
    * Remove unused assets
    
    - Remove unused script prototype: AudioDirector
    - Remove unused prefab: AudioDirector
    - Remove unused SoundFile asset
    - Remove AudioManager prefab from start and game scene
    
    * Update audio sources to use Audio Mixer Groups
    
    - Also update gitignore to ignore unity user settings
    
    * Update audio options to use audio mixer exposed parameters
    
    * Update gitignore with vscode dir
    
    * Remove options singleton use for volume in music scripts

--- Assets/Game Jam Version/Assets/Options/config_Options.cs
+++ Assets/Game Jam Version/Assets/Options/config_Options.cs
@@ -7,9 +7,4 @@ public class config_Options : ScriptableObject {
     [Range(0f, 1f)] public float musicVolume = 1f;
     [Range(0f, 1f)] public float sfxVolume = 1f;
     [Range(0, 1)] public int musicTrack = 0;
-
-    // Read-only example
-    // protected float secretValue = 5f;
-    // public float PublicValue { get { return secretValue; } }
-
 }
