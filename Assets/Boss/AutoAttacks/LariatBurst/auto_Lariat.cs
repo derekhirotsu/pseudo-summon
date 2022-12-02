db6821fb7dbfd953952f32cc6abab44db9b25b6d
Derek Hirotsu <55931997+derekhirotsu@users.noreply.github.com>
2022-12-01 16:46:51 -0800

    Update lariat boss attack to use new health class

--- Assets/Boss/AutoAttacks/LariatBurst/auto_Lariat.cs
+++ Assets/Boss/AutoAttacks/LariatBurst/auto_Lariat.cs
@@ -2,58 +2,85 @@ using PseudoSummon.Audio;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class auto_Lariat : MonoBehaviour
+namespace PseudoSummon
 {
-    [SerializeField] private float rotationSpeed = 200;
-    [SerializeField] private SoundFile hitSfx;
-    [SerializeField] private List<TestBullet> childProjectiles;
-
-    private HealthTracker health;
-    private AudioProvider _audio;
-
-    private float yRotation = 0;
-
-    private void Awake() {
-        _audio = GetComponent<AudioProvider>();
-        health = GetComponent<HealthTracker>();
-    }
-
-    private void Start() {
-        foreach (Transform child in transform) {
-            TestBullet projectile = child.GetComponent<TestBullet>();
-
-            if ( projectile != null) {
-                childProjectiles.Add(projectile);
-            }
-        }
-    }
-
-    private void FixedUpdate()
+    public class auto_Lariat : MonoBehaviour
     {
-        float diff = Time.fixedDeltaTime * rotationSpeed;
-        yRotation = (yRotation + diff) % 360;
-            
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yRotation, 0), 1f);
+        [SerializeField] private float rotationSpeed = 200;
+        [SerializeField] private SoundFile hitSfx;
+        [SerializeField] private List<TestBullet> childProjectiles;
 
-        if (health.TookDamage(consumeTrigger:true)) {
-            _audio.PlaySound(hitSfx);
+        private Health _health;
+        private AudioProvider _audio;
+
+        private float yRotation = 0;
+
+        private void Awake()
+        {
+            _audio = GetComponent<AudioProvider>();
+            _health = GetComponent<Health>();
         }
 
-        if (health.HealthPercentage <= 0) {
-            CastOutward();
+        private void OnEnable()
+        {
+            _health.CurrentHealthModified += OnCurrentHealthModified;
         }
-    }
 
-    private void CastOutward() {
-        transform.DetachChildren();
+        private void OnDisable()
+        {
+            _health.CurrentHealthModified -= OnCurrentHealthModified;
+        }
 
-        for (int i = childProjectiles.Count-1; i >= 0; --i) {
-            if (childProjectiles[i] != null) {
-                Vector3 outwardVector = (childProjectiles[i].transform.position - transform.position).normalized;
-                childProjectiles[i].SetDirection(outwardVector);
+        private void Start()
+        {
+            foreach (Transform child in transform)
+            {
+                TestBullet projectile = child.GetComponent<TestBullet>();
+
+                if (projectile != null)
+                {
+                    childProjectiles.Add(projectile);
+                }
             }
         }
 
-        Destroy(gameObject);
+        private void FixedUpdate()
+        {
+            float diff = Time.fixedDeltaTime * rotationSpeed;
+            yRotation = (yRotation + diff) % 360;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yRotation, 0), 1f);
+        }
+
+        private void CastOutward()
+        {
+            transform.DetachChildren();
+
+            for (int i = childProjectiles.Count - 1; i >= 0; --i)
+            {
+                if (childProjectiles[i] != null)
+                {
+                    Vector3 outwardVector = (childProjectiles[i].transform.position - transform.position).normalized;
+                    childProjectiles[i].SetDirection(outwardVector);
+                }
+            }
+
+            Destroy(gameObject);
+        }
+
+        private void OnCurrentHealthModified(int difference)
+        {
+            if (difference >= 0)
+            {
+                return;
+            }
+
+            _audio.PlaySound(hitSfx);
+
+            if (_health.CurrentHealth <= 0)
+            {
+                CastOutward();
+            }
+        }
     }
 }
